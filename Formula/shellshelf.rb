6ccf553a74f310a2356cf7a176060d818afee7cc
vpereira <vitorcali94@gmail.com>
2026-03-27 02:05:37 +0000

    Update shellshelf formula to build 11

--- Formula/shellshelf.rb
+++ Formula/shellshelf.rb
@@ -6,19 +6,19 @@ class Shellshelf < Formula
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/vcali/shellshelf/releases/download/v0.1.0-build.5/combib-aarch64-apple-darwin.tar.gz"
-      sha256 "614c56988d268f3af4a9d9935468ad3a13e829c176eea17a2de9397b703d649d"
+      url "https://github.com/vcali/shellshelf/releases/download/v0.1.0-build.11/shellshelf-aarch64-apple-darwin.tar.gz"
+      sha256 "9bfe50d9963ea36d1e2677b5d90abafd9becf77928b85cb2f568f2f7c1d8a7a5"
     else
-      url "https://github.com/vcali/shellshelf/releases/download/v0.1.0-build.5/combib-x86_64-apple-darwin.tar.gz"
-      sha256 "28ba7f002c8bb1d244e9b9bf717797f1cdfe0235320514b2f28b7309e6568d82"
+      url "https://github.com/vcali/shellshelf/releases/download/v0.1.0-build.11/shellshelf-x86_64-apple-darwin.tar.gz"
+      sha256 "989996bd5692676809fa9f0fdfad6fa7bfca717fac2f8747873bfffc1003059c"
     end
   elsif OS.linux?
-    url "https://github.com/vcali/shellshelf/releases/download/v0.1.0-build.5/combib-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "4bf077aeb6ed7e531bf3305d637e22b2b16aff3bf1c10866509d19daccc3e891"
+    url "https://github.com/vcali/shellshelf/releases/download/v0.1.0-build.11/shellshelf-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "57bec036feb4cba4fcfa8b408b05dced071240a02ce480ac178d82066066dd4a"
   end
 
   def install
-    bin.install "combib" => "shellshelf"
+    bin.install "shellshelf"
   end
 
   test do
